538945739b6e9ac395a4df6b7b0c59a912b5bdae
Pedro Chacon <pedro.chacon.14@sansano.usm.cl>
2018-11-06 18:56:39 -0300

    Algo se modificó

--- aeropuerto/clientenode.js
+++ aeropuerto/clientenode.js
@@ -28,12 +28,13 @@ var packageDefinition = protoLoader.loadSync(
     oneofs: true
   });
 
-  function msleep(n) {
-    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-  }
-  function sleep(n) {
-    msleep(n*1000);
-  }
+function msleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+
+function sleep(n) {
+  msleep(n * 1000);
+}
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).aeropuerto;
 
 var avion = {
@@ -47,32 +48,41 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 function solicitar_aterrizaje(nombre, proveniente) {
   var respuesta;
   var client = new hello_proto.Aeropuerto(avion.torre_inicial,
     grpc.credentials.createInsecure());
-    var user;
-    if (process.argv.length >= 3) {
-      user = process.argv[2];
-    } else {
-      user = nombre;
+  var user;
+  if (process.argv.length >= 3) {
+    user = process.argv[2];
+  } else {
+    user = nombre;
+  }
+  client.Pedir_Aterrizaje({
+    nombre: nombre,
+    proveniente: proveniente
+  }, function(err, response) {
+    console.log('%s', nombre);
+    respuesta = response.respuesta;
+    while (respuesta === 0) {
+      console.log(`[Avion - ${nombre}]: Todas las pistas estan asignadas, el avion predecesor es PLACEHOLDER...`);
+      sleep(10);
+      client.Pedir_Aterrizaje({
+        nombre: nombre,
+        proveniente: proveniente
+      }, (err, response) => {
+        respuesta = response.respuesta;
+      });
     }
-    client.Pedir_Aterrizaje({nombre: nombre, proveniente: proveniente}, function(err, response) {
-      console.log('%s',nombre);
-      respuesta = response.respuesta;
-    });
-      while (respuesta === 0) {
-        console.log(`[Avion - ${nombre}]: Todas las pistas estan asignadas, el avion predecesor es PLACEHOLDER...`);
-        sleep(10);
-        solicitar_aterrizaje(nombre,proveniente);
-      }
-      if (response.respuesta === 1) {
-        console.log(`[Avion - ${avion.numero}]: Está aterrizando en la pista: `, response.pista);
-      }
+    if (respuesta === 1) {
+      console.log(`[Avion - ${avion.numero}]: Está aterrizando en la pista: `, response.pista);
+    }
+  });
 }
 
 function main() {
-  solicitar_aterrizaje(avion.numero,avion.proveniente);
+  solicitar_aterrizaje(avion.numero, avion.proveniente);
 }
 rl.question('Numero de avion: ', (answer) => {
   avion.numero = answer;
