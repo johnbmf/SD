35ac8b714725b8bbe0ea85e0d9777fa9580f0903
johnbmf <jonathan.mendoza.14@sansano.usm.cl>
2018-11-07 15:38:33 -0300

    avance

--- aeropuerto/avion.py
+++ aeropuerto/avion.py
@@ -5,14 +5,25 @@ from concurrent import futures
 import aeropuerto_pb2
 import aeropuerto_pb2_grpc
 
+def cargar_combustible(avion):
+    avion.combustible = avion.combustible_max
+    print ("Combustible cargado exitosamente.\n")
+    return
+
+def ingresar_carga(avion):
+    avion.carga = int(input("[Avion - " + avion.nombre + "]: Ingrese carga del avion: "))
+
 class Avion:
     def __init__(self):
         print("Bienvenido al vuelo\n")
         self.nombre = str(input("Numero de avion: "))
         self.carga_max = int(input("[Avion - " + self.nombre + "]: Carga maxima en [Kg]: "))
+        self.carga = 0
         self.combustible_max = int(input("[Avion - " + self.nombre + "]: Capacidad del tanque de combustible en [L]: "))
+        self.combustible = self.combustible_max
         self.torre_control = str(input("[Avion - " + self.nombre + "]: Torre de control inicial: "))
         self.proveniente = "NOWHERE"
+        self.destino = "NOWHERE"
 
 
 def run():
@@ -20,18 +31,51 @@ def run():
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     avion = Avion()
-    channel = grpc.insecure_channel(avion.torre_control)  #localhost:50051
-    stub = aeropuerto_pb2_grpc.AeropuertoStub(channel)
+    loop = True
+    while (loop):
+        channel = grpc.insecure_channel(avion.torre_control)  #localhost:50051
+        stub = aeropuerto_pb2_grpc.AeropuertoStub(channel)
+        #Creacion de la solicitud de aterrizaje
+        mensaje = aeropuerto_pb2.AtRequest(nombre = avion.nombre, proveniente = avion.proveniente)
+        resp = stub.Pedir_Aterrizaje(mensaje)
+        print ("[Avion - " + avion.nombre + "]: Esperando pista de aterrizaje...\n")
 
-    #Creacion de la solicitud de aterrizaje
-    mensaje = aeropuerto_pb2.AtRequest(nombre = avion.nombre, proveniente = avion.proveniente)
-    resp = stub.Pedir_Aterrizaje(mensaje)
-    print ("[Avion - " + avion.nombre + "]: Esperando pista de aterrizaje...\n")
+        if (resp.respuesta == 0):
+            print("Todas las pistas ocupadas. Avion predecesor es " + str(resp.pos_cola))
+            while(resp.respuesta == 0):
+                time.sleep(10)
+                resp = stub.Pedir_Aterrizaje_Encolado(mensaje)
 
-    #caso de que si se puede aterrizar
-    if (resp.respuesta == 1):
-        print("[Avion - " + avion.nombre + "]: Aterrizando en la pista " + str(resp.pista) + "...\n")
+        if (resp.respuesta == 1):
+            print("[Avion - " + avion.nombre + "]: Aterrizando en la pista " + str(resp.pista) + "...\n")
 
 
+        loop_opciones = True
+        while (loop_opciones):
+            inp = int(input("Ingrese numero de tarea a ejecutar:\n1)Cargar combustible\n2)Ingresar cantidad de carga\n3)Solicitar despegue\n"))
+
+            if (inp == 1):
+                cargar_combustible(avion)
+            elif (inp == 2):
+                ingresar_carga(avion)
+            elif (inp == 3):
+                #crear mensaje
+                avion.destino = str(input("Ingrese destino: "))
+                mensaje = aeropuerto_pb2.DesRequest(nombre = avion.nombre, carga = avion.carga, carga_max = avion.carga_max,
+                                                    combustible = avion.combustible, combustible_max = avion.combustible_max,
+                                                    destino = avion.destino)
+                resp = stub.Solicitar_despegue(mensaje)
+
+                if (resp.respuesta == 0):
+                    print("Carga maxima excedida o el combustible no esta completamente cargado. Verifique estos parametros.")
+                if (resp.respuesta == 1):
+                    print("Pista " + str(resp.pista) + " asiganda, altura de " + str(resp.altura) + "\n")
+                    print("Despegando...\n")
+                    avion.proveniente = resp.nombre_torre
+                    avion.carga = 0
+                    avion.combustible = 0
+                    avion.torre_control = resp.torre_control
+                    loop_opciones = False
+                    input("Presione enter para solicitar aterrazaje en " + avion.destino)
 if __name__ == '__main__':
     run()
--- aeropuerto/torre.py
+++ aeropuerto/torre.py
@@ -4,6 +4,20 @@ from concurrent import futures
 
 import aeropuerto_pb2
 import aeropuerto_pb2_grpc
+import collections
+
+def abrir_puertos(aer_serv, server):
+    if (aer_serv.nombre in ["Santiago", 'santiago', 'SANTIAGO']):
+        server.add_insecure_port('[::]:50051')
+        server.add_insecure_port('[::]:50052')
+    elif (aer_serv.nombre in ["Antofagasta", 'antofagasta', 'ANTOFAGASTA']):
+        server.add_insecure_port('[::]:50061')
+        server.add_insecure_port('[::]:50062')
+    elif (aer_serv.nombre in ["Lima", 'lima', 'LIMA']):
+        server.add_insecure_port('[::]:50071')
+        server.add_insecure_port('[::]:50072')
+    return
+
 
 def crear_pistas(num_pistas_aterrizaje, num_pistas_despegue):
     pistas_aterrizaje = [None] * num_pistas_aterrizaje
@@ -16,6 +30,14 @@ def verificar_pista_disponible(lista_pistas):
             return i
     return -1;
 
+def check_destino(destino):
+    if (destino in ["Santiago", 'santiago', 'SANTIAGO']):
+        return "localhost:50051"
+    elif (destino in ["Antofagasta", 'antofagasta', 'ANTOFAGASTA']):
+        return "localhost:50061"
+    elif (destino in ["Lima", 'lima', 'LIMA']):
+        return "localhost:50071"
+
 class AeropuertoServicer(aeropuerto_pb2_grpc.AeropuertoServicer):
 
     def __init__(self):
@@ -24,6 +46,7 @@ class AeropuertoServicer(aeropuerto_pb2_grpc.AeropuertoServicer):
         self.num_pistas_aterrizaje = int(input("[Torre de control - " + self.nombre + "] Cantidad de pistas de aterrizaje: "))
         self.num_pistas_despegue = int(input("[Torre de control - " + self.nombre + "] Cantidad de pistas de despegue: "))
         self.pA, self.pD = crear_pistas(self.num_pistas_aterrizaje, self.num_pistas_despegue)
+        self.cola = collections.deque([])
 
         print (self.nombre)
 
@@ -40,14 +63,51 @@ class AeropuertoServicer(aeropuerto_pb2_grpc.AeropuertoServicer):
             return(respuesta)
         else:
             print("Todas las pistas ocupadas, encolando avion \n")
-            respuesta = aeropuerto_pb2.AtReply(respuesta = 0, pista = 0, altura = 5, pos_cola = 2)
+            self.cola.append(request.nombre)
+            respuesta = aeropuerto_pb2.AtReply(respuesta = 0, pista = 0, altura = 5*len(self.cola), pos_cola = 2)
             return(respuesta)
+
+    def Pedir_Aterrizaje_Encolado(self, request, context):
+        if (self.cola[0] == request.nombre):
+            pista_disponible = verificar_pista_disponible(self.pA)
+
+            if (pista_disponible == -1):
+                respuesta = aeropuerto_pb2.AtReply(respuesta = 0, pista = 0, altura = 5, pos_cola = 2)
+                return (respuesta)
+
+            else:
+                respuesta = aeropuerto_pb2.AtReply(respuesta=1, pista=pista_disponible, altura=5, pos_cola=0)
+                self.cola.popleft()
+                return (respuesta)
+
+        else:
+            respuesta = aeropuerto_pb2.AtReply(respuesta = 0, pista = 0, altura = 5, pos_cola = 2)
+            return (respuesta)
+
+    def Solicitar_despegue(self, request, context):
+        print("[Torre de control - " + self.nombre + "] " + request.nombre + " solicita un despegue.\n")
+        #Verifica combustible lleno
+        if (request.combustible < request.combustible_max):
+            print("[Torre de control - " + self.nombre + "] " + request.nombre + " no cumple con el combustible solicitado\n")
+            respuesta = aeropuerto_pb2.DesReply(respuesta = 0, pista = 0, altura = 0, nombre_torre = self.nombre,
+                                                torre_control = "")
+        elif (request.carga > request.carga_max):
+            print("[Torre de control - " + self.nombre + "] " + request.nombre + " no cumple con carga maxima.\n")
+            respuesta = aeropuerto_pb2.DesReply(respuesta = 0, pista = 0, altura = 0, nombre_torre = self.nombre,
+                                                torre_control = "")
+        else:
+            puerto_destino = check_destino(request.destino)
+            respuesta = aeropuerto_pb2.DesReply(respuesta = 1, pista = 1, altura = 10, nombre_torre = self.nombre,
+                                                torre_control = puerto_destino)
+            self.pA[self.pA.index(request.nombre)] = None
+            print("[Torre de control - " + self.nombre + "] " + request.nombre + " despego del aeropuerto.\n")
+        return respuesta
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    aeropuerto_pb2_grpc.add_AeropuertoServicer_to_server(AeropuertoServicer(), server)
-    server.add_insecure_port('[::]:50051')
-    server.add_insecure_port('[::]:50052')
+    aer_serv = AeropuertoServicer()
+    aeropuerto_pb2_grpc.add_AeropuertoServicer_to_server(aer_serv, server)
+    abrir_puertos(aer_serv, server)
     server.start()
     try:
         while True:
